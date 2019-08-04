8b8ba6d09e61bc61fd60860704b2c0ca6e1fccf0
Mevericks <reloved@gmail.com>
2019-08-04 23:13:48 +0300

    прописал методы для моделей User-Role-Permission

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+	public function users(){
+		return $this->belongsToMany('App\User','role_user');
+	}
+	public function perms(){
+		return $this->belongsToMany('App\Permission','permission_role');
+	}
 }
--- app/User.php
+++ app/User.php
@@ -32,4 +32,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles(){
+    	return $this->belongsToMany('App\Role','role_user');
+	}
 }
